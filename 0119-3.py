4ab4a97e2ab938825cb7c7574072b843b55c2141
Lu102777 <kc20011027@gmail.com>
2023-01-24 12:52:12 +0800

    aaa

--- 0119-3.py
+++ 0119-3.py
@@ -1,11 +1,11 @@
 import requests
 
-url="https://movie.douban.com/j/chart/top_list"
-
-param={
-    "type":"24",
-    "interval_id":"100:90",
-    "action":"",
-    "start":0,
-    "limit":20,
+url = "https://movie.douban.com/j/chart/top_list"
+# 重新封裝參數
+param = {
+    "type": "24",
+    "interval_id": "100:90",
+    "action": "",
+    "start": 0,
+    "limit": 20,
 }
