ca4f199767dbcd17f996d90cea9101e56c7460a6
Lu102777 <kc20011027@gmail.com>
2023-01-24 12:58:17 +0800

    aaa

--- 0119-3.py
+++ 0119-3.py
@@ -9,3 +9,11 @@ param = {
     "start": 0,
     "limit": 20,
 }
+
+header = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+}
+
+resp = requests.get(url=url, params=param, headers=header)
+
+print(resp.json())
